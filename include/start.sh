ebcb18e509ce4e9edc36ccfc3e4e4f6881d37779
8ctopus <hello@octopuslabs.io>
2020-04-18 07:44:25 +0500

    cleanup: script logging

--- include/start.sh
+++ include/start.sh
@@ -12,44 +12,44 @@ fi
 echo "domain: $DOMAIN"
 echo "document root: $DOCUMENT_ROOT"
 
-# check if we should expose apache2 to host
+# check if we should expose apache to host
 if [ -d /docker/etc/ ];
 then
-    echo "Expose apache2 to host..."
+    echo "Expose apache to host..."
     sleep 3
 
-    # check if apache2 config exists on host
+    # check if apache config exists on host
     if [ -z "$(ls -A /docker/etc/apache2/ 2> /dev/null)" ];
     then
         # config doesn't exist on host
-        echo "Expose apache2 to host - no host config"
+        echo "Expose apache to host - no host config"
 
         # check if config backup exists
         if [ -d /etc/apache2.bak/ ];
         then
             # restore config from backup
-            echo "Expose apache2 to host - restore config from backup"
+            echo "Expose apache to host - restore config from backup"
             rm /etc/apache2/
             cp -r /etc/apache2.bak/ /etc/apache2/
         else
             # create config backup
-            echo "Expose apache2 to host - backup container config"
+            echo "Expose apache to host - backup container config"
             cp -r /etc/apache2/ /etc/apache2.bak/
         fi
 
         # copy config to host
-        echo "Expose apache2 to host - copy config to host"
+        echo "Expose apache to host - copy config to host"
         cp -r /etc/apache2/ /docker/etc/
     else
-        echo "Expose apache2 to host - config exists on host"
+        echo "Expose apache to host - config exists on host"
     fi
 
     # create symbolic link so host config is used
-    echo "Expose apache2 to host - create symlink"
+    echo "Expose apache to host - create symlink"
     rm -rf /etc/apache2/
     ln -s /docker/etc/apache2 /etc/apache2
 
-    echo "Expose apache2 to host - OK"
+    echo "Expose apache to host - OK"
 
     if [ ! -e /etc/ssl/apache2/$DOMAIN.pem ];
     then
@@ -64,7 +64,7 @@ then
         echo "Generate self-signed SSL certificate for $DOMAIN - OK"
     fi
 
-    echo "Configure apache2 for domain..."
+    echo "Configure apache for domain..."
 
     # set document root dir
     sed -i "s|/var/www/localhost/htdocs|/var/www/site$DOCUMENT_ROOT|g" /etc/apache2/httpd.conf
@@ -75,43 +75,43 @@ then
     sed -i "s|#ServerName .*:80|ServerName $DOMAIN:80|g" /etc/apache2/httpd.conf
     sed -i "s|ServerName .*:443|ServerName $DOMAIN:443|g" /etc/apache2/conf.d/ssl.conf
 
-    echo "Configure apache2 for domain - OK"
+    echo "Configure apache for domain - OK"
 
-    echo "Expose php7 to host..."
+    echo "Expose php to host..."
     sleep 3
 
-    # check if php7 config exists on host
+    # check if php config exists on host
     if [ -z "$(ls -A /docker/etc/php7/ 2> /dev/null)" ];
     then
         # config doesn't exist on host
-        echo "Expose php7 to host - no host config"
+        echo "Expose php to host - no host config"
 
         # check if config backup exists
         if [ -d /etc/php7.bak/ ];
         then
             # restore config from backup
-            echo "Expose php7 to host - restore config from backup"
+            echo "Expose php to host - restore config from backup"
             rm /etc/php7/
             cp -r /etc/php7.bak/ /etc/php7/
         else
             # create config backup
-            echo "Expose php7 to host - backup container config"
+            echo "Expose php to host - backup container config"
             cp -r /etc/php7/ /etc/php7.bak/
         fi
 
         # copy config to host
-        echo "Expose php7 to host - copy config to host"
+        echo "Expose php to host - copy config to host"
         cp -r /etc/php7/ /docker/etc/
     else
-        echo "Expose php7 to host - config exists on host"
+        echo "Expose php to host - config exists on host"
     fi
 
     # create symbolic link so host config is used
-    echo "Expose php7 to host - create symlink"
+    echo "Expose php to host - create symlink"
     rm -rf /etc/php7/
     ln -s /docker/etc/php7 /etc/php7
 
-    echo "Expose php7 to host - OK"
+    echo "Expose php to host - OK"
 fi
 
 # create xdebug log file
@@ -138,10 +138,10 @@ fi
 
 echo "-----------------------------------------------"
 
-# start apache2
+# start apache
 httpd -k start
 
-# check if apache2 is running
+# check if apache is running
 if pgrep -x httpd > /dev/null
 then
     echo "Start container web server - OK - ready for connections"
@@ -155,10 +155,10 @@ stop_container()
     echo ""
     echo "Stop container web server... - received SIGTERM signal"
 
-    # stop apache2
-    echo "Stop apache2..."
+    # stop apache
+    echo "Stop apache..."
     httpd -k stop
-    echo "Stop apache2 - OK"
+    echo "Stop apache - OK"
 
     echo "Stop container web server - OK"
     exit
@@ -168,7 +168,7 @@ stop_container()
 # https://unix.stackexchange.com/questions/317492/list-of-kill-signals
 trap stop_container SIGTERM
 
-restart_apache2()
+restart_processes()
 {
     sleep 0.5
 
@@ -176,41 +176,41 @@ restart_apache2()
     if php-fpm7 -t
     then
         # restart php-fpm
-        echo "Restart php-fpm7..."
+        echo "Restart php-fpm..."
         httpd -k restart
 
         # check if php-fpm is running
         if pgrep -x php-fpm7 > /dev/null
         then
-            echo "Restart php-fpm7 - OK"
+            echo "Restart php-fpm - OK"
         else
-            echo "Restart php-fpm7 - FAILED"
+            echo "Restart php-fpm - FAILED"
         fi
     else
-        echo "Restart php-fpm7 - FAILED - syntax error"
+        echo "Restart php-fpm - FAILED - syntax error"
     fi
 
-    # test apache2 config
+    # test apache config
     if httpd -t
     then
-        # restart apache2
-        echo "Restart apache2..."
+        # restart apache
+        echo "Restart apache..."
         httpd -k restart
 
-        # check if apache2 is running
+        # check if apache is running
         if pgrep -x httpd > /dev/null
         then
-            echo "Restart apache2 - OK"
+            echo "Restart apache - OK"
         else
-            echo "Restart apache2 - FAILED"
+            echo "Restart apache - FAILED"
         fi
     else
-        echo "Restart apache2 - FAILED - syntax error"
+        echo "Restart apache - FAILED - syntax error"
     fi
 }
 
 # infinite loop, will only stop on termination signal
 while true; do
-    # restart apache2 if any file in /etc/apache2 and /etc/php7 changes
-    inotifywait --quiet --event modify,create,delete --timeout 3 --recursive /etc/apache2/ /etc/php7/ && restart_apache2
+    # restart apache and php-fpm if any file in /etc/apache2 or /etc/php7 changes
+    inotifywait --quiet --event modify,create,delete --timeout 3 --recursive /etc/apache2/ /etc/php7/ && restart_processes
 done
