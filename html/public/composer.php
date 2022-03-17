049097289157cfe6fca75e119885f45ee3c9633d
8ctopus <hello@octopuslabs.io>
2022-03-17 09:52:59 +0500

    added: composer example

---
+++ html/public/composer.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Simple example that shows how to use composer
+ * In this case we include [Whoops](https://github.com/filp/whoops) which is a php error handler
+ */
+
+$autoLoad = '../vendor/autoload.php';
+
+if (!file_exists($autoLoad)) {
+    echo 'please run docker exec web composer install';
+    exit;
+}
+
+// include composer dependencies
+require_once $autoLoad;
+
+// create whoops object
+$whoops = new \Whoops\Run();
+$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+$whoops->register();
+
+// let's generate a division by zero to showcase whoops
+$a = 1;
+$b = 0;
+
+$c = $a / $b;
