c9012bda7c3d9cb7c97c687536ae7f9f95137f17
8ctopus <hello@octopuslabs.io>
2022-09-23 15:22:49 +0500

    updated: alpine from 3.16.0 to 3.16.2

--- php-spx/build.sh
+++ php-spx/build.sh
@@ -4,8 +4,8 @@ DIR="$( cd "$( dirname "$0" )" && pwd )"
 # build for ubuntu 20.04
 #DOCKER_BUILDKIT=1 docker build --file $DIR/Dockerfile-ubuntu --build-arg "VERSION=20.04" --output type=local,dest=lib .
 
-# build for alpine 3.15.4
-DOCKER_BUILDKIT=1 docker build --file $DIR/Dockerfile-alpine --build-arg "VERSION=3.15.4" --output type=local,dest=$DIR/lib .
+# build for alpine 3.16.2
+#DOCKER_BUILDKIT=1 docker build --file $DIR/Dockerfile-alpine --build-arg "VERSION=3.16.2" --output type=local,dest=$DIR/lib .
 
 # build for alpine edge
 DOCKER_BUILDKIT=1 docker build --file $DIR/Dockerfile-alpine --build-arg "VERSION=edge" --output type=local,dest=$DIR/lib .
