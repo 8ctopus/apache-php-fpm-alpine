737768451348cb33126a5e439c2edafa8b9915a5
8ctopus <hello@octopuslabs.io>
2022-09-30 10:27:54 +0500

    deleted: examples (moved to php sandbox)

--- html/public/index.php
+++ html/public/index.php
@@ -1,32 +1,7 @@
 <?php
 
-/**
- * List all examples
- */
+$hostname = gethostname();
 
-require_once '../header.php';
+echo "Hello from {$hostname}!";
 
-echo 'Hello from docker container!' . PHP_EOL . PHP_EOL;
-
-// list current directory files
-$it = new RecursiveDirectoryIterator(__DIR__, FilesystemIterator::SKIP_DOTS);
-
-while ($it->valid()) {
-    if (!$it->isDot()) {
-        $file = $it->getSubPath() .'/'. $it->getSubPathName();
-
-        switch ($it->getSubPathName()) {
-            case 'index.php':
-            case 'favicon.ico':
-                break;
-
-            default:
-                echo "<a href=\"{$file}\">{$file}</a>" . PHP_EOL;
-                break;
-        }
-    }
-
-    $it->next();
-}
-
-require_once '../footer.php';
+phpinfo();
