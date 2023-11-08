0c92e279c24505c75489035399bd9fe572e18dee
8ctopus <hello@octopuslabs.io>
2023-11-08 11:17:48 +0400

    Update self sign installation script

--- include/selfsign.sh
+++ include/selfsign.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-EXPECTED_CHECKSUM="$(php -r 'copy("https://github.com/8ctopus/self-sign/raw/master/bin/selfsign.sha256", "php://stdout");')"
-php -r "copy('https://github.com/8ctopus/self-sign/raw/master/bin/selfsign.phar', 'selfsign.phar');"
-ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha256', 'selfsign.phar');")"
+EXPECTED_CHECKSUM="$(php -r 'copy("https://github.com/8ctopus/self-sign/releases/download/0.1.8/selfsign.sha256", "php://stdout");')"
+#echo $EXPECTED_CHECKSUM
+php -r "copy('https://github.com/8ctopus/self-sign/releases/download/0.1.8/selfsign.phar', '/selfsign.phar');"
+ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha256', '/selfsign.phar');")"
 
 if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
 then
-    >&2 echo 'ERROR: Invalid checksum'
-    rm selfsign.phar
+    echo 'ERROR: Invalid checksum'
+    rm /selfsign.phar
     exit 1
+else
+    exit 0
 fi
-
-exit 0
